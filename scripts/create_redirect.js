21f12b087b6fd88361a46843b57e018e2a741504
Yotam Mann <yotammann@gmail.com>
2020-04-29 16:29:57 -0400

    creating redirects on build

---
+++ scripts/create_redirect.js
@@ -0,0 +1,25 @@
+const { execSync } = require('child_process')
+const { resolve, relative, dirname } = require('path')
+const glob = require('glob-promise')
+const fs = require('fs-extra')
+
+function createRedirects(version, tag) {
+	console.log(`creating redirects for ${version}`)
+	const basePath = resolve(__dirname, '../docs/', version)
+	const docFiles = glob.sync(resolve(basePath, "**/*.html"))
+	docFiles.forEach(file => {
+		const latestFile = file.replace(version, tag)
+		fs.ensureDirSync(dirname(latestFile))
+		fs.writeFileSync(latestFile, `
+			<meta http-equiv="refresh" content="0; URL='${relative(dirname(latestFile), file)}'" />
+		`)
+	})
+}
+
+// create redirect for 'latest'
+const latestVersion = execSync(`npm show tone@latest version`).toString().trim()
+createRedirects(latestVersion, 'latest')
+
+// do that for 'next tag
+const nextVersion = execSync(`npm show tone@next version`).toString().trim()
+createRedirects(nextVersion, 'next')
