ad7d7a2f3eff0727cd8eb23fde99815ff88a1424
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-17 02:54:47 +0000

    Deploy Tonejs/tonejs.github.io to github.com/Tonejs/tonejs.github.io.git:master

--- next/build/esm/component/envelope/Envelope.js
+++ next/build/esm/component/envelope/Envelope.js
@@ -300,7 +300,7 @@ export class Envelope extends ToneAudioNode {
      * @example
      * const env = new Tone.Envelope(0.5, 1, 0.4, 2);
      * env.triggerAttackRelease(2);
-     * setInterval(() => console.log(env.getValueAtTime), 100);
+     * setInterval(() => console.log(env.getValueAtTime(Tone.now())), 100);
      */
     getValueAtTime(time) {
         return this._sig.getValueAtTime(time);
--- next/build/esm/version.js
+++ next/build/esm/version.js
@@ -1,2 +1,2 @@
-export const version = "14.7.53";
+export const version = "14.7.55";
 //# sourceMappingURL=version.js.map
