6442ba0e5ef7492adc19c0b499ac8ccc8b7a6a93
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-28 04:53:25 +0000

    Deploy Tonejs/tonejs.github.io to github.com/Tonejs/tonejs.github.io.git:master

--- next/build/esm/core/context/ToneAudioNode.js
+++ next/build/esm/core/context/ToneAudioNode.js
@@ -323,4 +323,20 @@ export function disconnect(srcNode, dstNode, outputNumber = 0, inputNumber = 0) 
         srcNode.disconnect();
     }
 }
+/**
+ * Connect the output of one or more source nodes to a single destination node
+ * @param nodes One or more source nodes followed by one destination node
+ * @example
+ * const player = new Tone.Player("https://tonejs.github.io/audio/drum-samples/conga-rhythm.mp3");
+ * const player1 = new Tone.Player("https://tonejs.github.io/audio/drum-samples/conga-rhythm.mp3");
+ * const filter = new Tone.Filter("G5").toDestination();
+ * // connect nodes to a common destination
+ * fanIn(player, player1, filter);
+ */
+export function fanIn(...nodes) {
+    const dstNode = nodes.pop();
+    if (isDefined(dstNode)) {
+        nodes.forEach(node => connect(node, dstNode));
+    }
+}
 //# sourceMappingURL=ToneAudioNode.js.map
--- next/build/esm/version.js
+++ next/build/esm/version.js
@@ -1,2 +1,2 @@
-export const version = "14.8.17";
+export const version = "14.8.19";
 //# sourceMappingURL=version.js.map
